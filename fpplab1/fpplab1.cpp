aabc35077d54c78ab89a68e86ab0b95ce8bcad61
TURUNDAM <turundamss@gmail.com>
2022-06-01 10:41:18 +0800

    fix

--- fpplab1/fpplab1.cpp
+++ fpplab1/fpplab1.cpp
@@ -13,14 +13,14 @@ int main()
 {
 	srand(time(NULL));
 
-	int n, m, q, i, j;
+	int n, m;
 	cout << "Enter size of vector A: ";
 	cin >> n;
 	cout << "Enter size of vector B: ";
 	cin >> m;
 	
 	vector <int> vector_a;
-	for (i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		vector_a.push_back (rand() % 20 + 1);
 	}
 /*	cout << "Vector A: " << endl;
@@ -31,7 +31,7 @@ int main()
 	}
 	*/
 	vector <int> vector_b;
-	for (i = 0; i < m; i++) {
+	for (int i = 0; i < m; i++) {
 		vector_b.push_back(rand() % 20 + 1);
 	}
 /*	cout << endl << "Vector B: " << endl;
@@ -45,24 +45,23 @@ int main()
 	double time_of_openmp1 = omp_get_wtime();
 
 #pragma omp paralel for 
-	for (auto a : vector_a)
-		for (auto b : vector_b)
-			result1+=a*b;
+	for (int i =0;i<vector_a.size()&&i<vector_b.size();i++) 
+			result1 += vector_a[i] * vector_b[i];
+	
 	double time_of_openmp2 = omp_get_wtime();
 
 	cout << "Time of compiling with OpenMP = " << time_of_openmp2 - time_of_openmp1;
 
 	int result2 = 0;
 	double time_of_function1 = omp_get_wtime();
-	for (auto a : vector_a)
-		for (auto b : vector_b)
-			result2 += a * b;
+	for (int i = 0; i < vector_a.size() && i < vector_b.size(); i++)
+		result2 += vector_a[i] * vector_b[i];
 	double time_of_function2 = omp_get_wtime();
 
 	cout << endl << "Time of compiling without OpenMP = " << time_of_function2 - time_of_function1;
 
 	if (result1 == result2)
-	cout << endl << "Multiplication of Vector A & B = " << result1;
+	cout << endl << "Scallar multiplication of Vector A & B = " << result1;
 		
 	return 0;
 }
